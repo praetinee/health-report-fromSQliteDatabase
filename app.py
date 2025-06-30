e2dcade0b0608812ce67820332d013d191648d7b
praetinee <praetinee@gmail.com>
2025-06-30 10:09:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import io
 import tempfile
 
-@st.cache(ttl=600)
+@st.cache_data(ttl=600)
 def load_sqlite_data():
     try:
         file_id = "1HruO9AMrUfniC8hBWtumVdxLJayEc1Xr"
