27912075dc905f8f9c8231eb11751af15073f604
praetinee <praetinee@gmail.com>
2025-07-04 15:11:48 +0700

    Update report_header.py

--- ui/report_header.py
+++ ui/report_header.py
@@ -6,7 +6,7 @@ if PROJECT_ROOT not in sys.path:
     sys.path.append(PROJECT_ROOT)
 
 import streamlit as st
-from utils import format_thai_date, parse_date_thai, get_float, interpret_bp
+from utils import format_thai_date, parse_date_thai, get_float
 
 def render_report_header(person):
     raw_date = parse_date_thai(person["วันที่ตรวจ"])
