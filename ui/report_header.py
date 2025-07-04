816eea7a19008d4f101cc550bdb7a4b9e532075a
praetinee <praetinee@gmail.com>
2025-07-04 11:58:41 +0700

    Update report_header.py

--- ui/report_header.py
+++ ui/report_header.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-# ✅ เพิ่ม path โฟลเดอร์หลัก เพื่อให้ Python หา utils.py เจอ
+# ✅ เพิ่ม path โฟลเดอร์หลัก เพื่อให้ Python หา utils.py ได้
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import streamlit as st
