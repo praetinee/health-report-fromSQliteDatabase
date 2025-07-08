f2a51c3bdaae8caf986f7ad243f16c8f3bc1d624
praetinee <praetinee@gmail.com>
2025-07-08 13:03:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ def summarize_liver(alp_val, sgot_val, sgpt_val):
             return "การทำงานของตับสูงกว่าเกณฑ์ปกติเล็กน้อย"
         return "ปกติ"
     except:
-        return ""
+        return "-"
 
 def liver_advice(summary_text):
     if summary_text == "การทำงานของตับสูงกว่าเกณฑ์ปกติเล็กน้อย":
@@ -845,10 +845,10 @@ st.markdown("""
         font-size: 22px !important; /* Slightly larger than body text */
     }
 
-    /* Control spacing for all elements in header */
+    /* Control spacing for all elements in header to be more spread out */
     .report-header-container * {
-        line-height: 1.7 !important; /* << ADJUST THIS VALUE FOR SPACING */
-        margin: 0.15rem 0 !important; /* Add small vertical margin between elements */
+        line-height: 1.9 !important; /* << ADJUST THIS VALUE FOR SPACING */
+        margin: 0.3rem 0 !important; /* Add vertical margin between elements */
         padding: 0 !important;
     }
 
