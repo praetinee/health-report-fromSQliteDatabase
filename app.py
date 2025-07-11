9859659052afb2b39b14cf8e5dbff6e717e01f57
praetinee <praetinee@gmail.com>
2025-07-11 18:59:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1463,15 +1463,3 @@ if "person_row" in st.session_state:
             </div>
         </div>
         """, unsafe_allow_html=True)
-" and am asking a query about/based on this code below.
-Instructions to follow:
-  * Don't output/edit the document if the query is Direct/Simple. For example, if the query asks for a simple explanation, output a direct answer.
-  * Make sure to **edit** the document if the query shows the intent of editing the document, in which case output the entire edited document, **not just that section or the edits**.
-    * Don't output the same document/empty document and say that you have edited it.
-    * Don't change unrelated code in the document.
-  * Don't output  and  in your final response.
-  * Any references like "this" or "selected code" refers to the code between  and  tags.
-  * Just acknowledge my request in the introduction.
-  * Make sure to refer to the document as "Canvas" in your response.
-
-Please make the search bar look like the provided ima
