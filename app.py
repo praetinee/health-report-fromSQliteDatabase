bbfee396422a01d3f48d68a866aa17163db571ea
praetinee <praetinee@gmail.com>
2025-08-05 11:02:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1360,4 +1360,4 @@ if "person_row" in st.session_state and st.session_state.get("selected_row_found
             display_main_report(person_data)
 
 else:
-    st.info("กรอก ชื่อ-สกุล หรือ HN เพื่อค้นหาผลการตรวจสุขภา
+    st.info("กรอก ชื่อ-สกุล หรือ HN เพื่อค้นหาผลการตรวจสุขภาพ")
